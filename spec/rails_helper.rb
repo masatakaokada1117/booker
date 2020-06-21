288112be968c3f7f4e235c1efe1049e3bab0d1c7
Masataka Okada <masataka.okada1117@gmail.com>
2020-06-21 23:38:42 +0900

    macrosの設定を編集

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,10 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+require 'devise'
+require File.expand_path('spec/support/controller_macros.rb')
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
