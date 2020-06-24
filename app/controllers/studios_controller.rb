aae59e1aeeda5295b4f984a2717bae575fb0e9e5
Masataka Okada <masataka.okada1117@gmail.com>
2020-06-24 23:39:35 +0900

    StudioControllerに追記

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,7 +1,17 @@
 class StudiosController < ApplicationController
+  before_action :set_studio, only: %i[show]
+
   def index
+    @studios = Studio.all.page(params[:page])
   end
 
   def show
+    @rooms = @studio.rooms
+  end
+
+  private
+
+  def set_studio
+    @studio = Studio.find(params[:id])
   end
 end
