131c2c590d2534d5e11675f375bbe8cda7e7fb77
Masataka Okada <masataka.okada1117@gmail.com>
2020-06-25 00:46:01 +0900

    rubocopでレイアウト修正

--- spec/requests/studios_request_spec.rb
+++ spec/requests/studios_request_spec.rb
@@ -1,19 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe "Studios", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/studios/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/studios/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+RSpec.describe 'Studios', type: :request do
 end
