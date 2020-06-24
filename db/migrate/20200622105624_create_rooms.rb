b0da5cb5696ec86f1733b55a14fa59a6a34773c6
Masataka Okada <masataka.okada1117@gmail.com>
2020-06-24 23:32:16 +0900

    Roomモデルの追加

---
+++ db/migrate/20200622105624_create_rooms.rb
@@ -0,0 +1,10 @@
+class CreateRooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :rooms do |t|
+      t.string :name, null: false
+      t.references :studio, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_145828) do
+ActiveRecord::Schema.define(version: 2020_06_22_105624) do
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "studio_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["studio_id"], name: "index_rooms_on_studio_id"
+  end
 
   create_table "studios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_06_21_145828) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms", "studios"
 end
